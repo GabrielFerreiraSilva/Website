5759717ed4af5c6e4875dab31d81fcd659d74c45
Gabriel Ferreira da Silva <126522573+GabrielFerreiraSilva@users.noreply.github.com>
2023-07-05 11:18:17 -0300

    Menu changes
    Improved the sidebar menu interaction

--- scripts/script-sidebar.js
+++ scripts/script-sidebar.js
@@ -1,9 +1,19 @@
 const btn_menu = document.querySelector(".btn-menu");
 
+const header_btn_menu = document.querySelector(".header-btn-menu");
+
 const sidebar_container = document.querySelector(".sidebar-container");
 
 btn_menu.addEventListener("click", function () {
 
-sidebar_container.classList.toggle("expand");
+    sidebar_container.classList.toggle("expand");
+
+});
+
+header_btn_menu.addEventListener("click", function () {
+
+
+
+    sidebar_container.classList.toggle("expand");
 
 });
